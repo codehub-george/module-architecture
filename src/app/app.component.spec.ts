b5ce07a38b8dbbc0928d8526fcf685024eff0f63
George Oiko <drg159@hotmail.com>
2019-11-02 08:01:32 +0200

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'module-architecture'`, () => {
+  it(`should have as title 'module-split'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('module-architecture');
+    expect(app.title).toEqual('module-split');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('module-architecture app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to module-split!');
   });
 });
